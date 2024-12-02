d90dd266139176c8f7ada5500404ee8e30ca5d88
Arnoldas nesvarbu <arnasmoz@gmail.com>
2024-12-02 18:53:16 +0200

    Day2

---
+++ 2024/Day2/Solution.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC._2024.Day2
+{
+    public class Solution
+    {
+        public int Solution1(List<string> input)
+        {
+            int safeReports = 0;
+            foreach (var item in input)
+            {
+                int[] reportData = item.Split(" ").Select(Int32.Parse).ToArray();
+                bool isDecreasing = false;
+                for (int i = 1; i < reportData.Length; i++)
+                {
+
+                    int diff = reportData[i] - reportData[i - 1];
+
+                    if (i == 1)
+                    {
+                        isDecreasing = diff < 0 ? true : false;
+                    }
+
+                    if (isDecreasing && diff >= 0 || diff < -3)
+                        break;
+                    else if (!isDecreasing && diff <= 0 || diff > 3)
+                        break;
+
+                    if (i == reportData.Length - 1)
+                        safeReports++;
+
+                }
+            }
+            return safeReports;
+        }
+
+        public int Solution2(List<string> input)
+        {
+            int safeReports = 0;
+            foreach (var item in input)
+            {
+                List<int> reportData = item.Split(" ").Select(Int32.Parse).ToList();
+                if(Check(reportData))
+                    safeReports++;
+                else 
+                {
+                    for(int i = 0; i < reportData.Count; i++)
+                    {
+                        var removedDupe = reportData.ToList();
+                        removedDupe.RemoveAt(i);
+                        if(Check(removedDupe))
+                        {
+                            safeReports++;
+                            break;
+                        }
+                    }
+                }
+                
+            }
+            return safeReports;
+        }
+
+        private bool Check(List<int> input) 
+        {
+            bool isDecreasing = false;
+            for (int i = 1; i < input.Count; i++)
+            {
+
+                int diff = input[i] - input[i - 1];
+
+                if (i == 1)
+                {
+                    isDecreasing = diff < 0 ? true : false;
+                }
+
+                if (isDecreasing && diff >= 0 || diff < -3)
+                    return false;
+                else if (!isDecreasing && diff <= 0 || diff > 3)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,21 @@
 ﻿using AOC;
-using AOC._2024.Day1;
+using Day1 = AOC._2024.Day1;
+using Day2 = AOC._2024.Day2;
 
 
-Day1();
+//Day1();
+Day2();
 
 void Day1() 
 {
-    Solution day1 = new Solution();
+    Day1.Solution day1 = new Day1.Solution();
     Console.WriteLine(day1.Solution1(InputReader.ReadToList()));
     Console.WriteLine(day1.Solution2(InputReader.ReadToList()));
+}
+
+void Day2() 
+{
+    Day2.Solution day2 = new Day2.Solution();
+    Console.WriteLine(day2.Solution1(InputReader.ReadToList()));
+    Console.WriteLine(day2.Solution2(InputReader.ReadToList()));
 }
