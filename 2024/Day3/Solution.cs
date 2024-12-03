601dd18ed5cbf681c73b5ed34e2b6cedb0a06a99
Arnoldas nesvarbu <arnasmoz@gmail.com>
2024-12-03 15:14:39 +0200

    Advent of code day3

---
+++ 2024/Day3/Solution.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AOC._2024.Day3
+{
+    public class Solution
+    {
+        public int Solution1(string input) 
+        {
+            int answer = 0;
+            input = input.Replace(Environment.NewLine, "");
+            string pattern = "(?:mul\\(\\d{1,3},\\d{1,3}\\))";
+            string paramPattern = "\\d{1,3}";
+            Regex regex = new Regex(pattern);
+            Regex paramRegex = new Regex(paramPattern);
+            MatchCollection commands = regex.Matches(input);
+
+
+            foreach (Match match in commands) {
+                var parameters = paramRegex.Matches(match.ToString());
+                answer += mul(int.Parse(parameters[0].ToString()), int.Parse(parameters[1].ToString()));
+             }
+
+            return answer;
+        }
+
+
+        public int Solution2(string input)
+        {
+            int answer = 0;
+            input = input.Replace(Environment.NewLine, "");
+            input = Regex.Replace(input, "(?:don't\\(\\))(.*?)(?:do\\(\\))", string.Empty);
+
+            string pattern = "(?:mul\\(\\d{1,3},\\d{1,3}\\))";
+            string paramPattern = "\\d{1,3}";
+            Regex regex = new Regex(pattern);
+            Regex paramRegex = new Regex(paramPattern);
+            MatchCollection commands = regex.Matches(input);
+
+            foreach (Match match in commands)
+            {
+                var parameters = paramRegex.Matches(match.ToString());
+                answer += mul(int.Parse(parameters[0].ToString()), int.Parse(parameters[1].ToString()));
+            }
+
+            return answer;
+        }
+
+        public int mul(int x, int y) 
+        {
+            return x * y;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
 ﻿using AOC;
 using Day1 = AOC._2024.Day1;
 using Day2 = AOC._2024.Day2;
+using Day3 = AOC._2024.Day3;
 
 
 //Day1();
-Day2();
+//Day2();
+Day3();
 
 void Day1() 
 {
@@ -18,4 +20,11 @@ void Day2()
     Day2.Solution day2 = new Day2.Solution();
     Console.WriteLine(day2.Solution1(InputReader.ReadToList()));
     Console.WriteLine(day2.Solution2(InputReader.ReadToList()));
+}
+
+void Day3()
+{
+    Day3.Solution day3 = new Day3.Solution();
+    Console.WriteLine(day3.Solution1(InputReader.Read()));
+    Console.WriteLine(day3.Solution2(InputReader.Read()));
 }
